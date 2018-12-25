c0c1febbd5425c9b7d71da5e20908c61275531ce
NTGOLjq <1392889987@qq.com>
2018-12-25 19:09:34 +0800

    12.25 19:09

---
+++ Cal_ViewTests/OperatorModel/class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cal_ViewTests.OperatorModel
+{
+    class Class1
+    {
+        public static void Main(string[] args)
+        {
+            decimal i1 = 0;
+        }
+    }
+}
